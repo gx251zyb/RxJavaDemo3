d5d387035d1f47bff3906ad7d4594db5c33d7654
gx251zyb <1123221743@qq.com>
2017-04-01 11:22:13 +0800

    MainActivity测试类提交

--- app/src/main/java/com/example/administrator/rxjavademo3/MainActivity.java
+++ app/src/main/java/com/example/administrator/rxjavademo3/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.administrator.rxjavademo3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java_rx3.RxUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * 调用create方法
+     * @param view
+     */
+
+    public void createMethod(View view) {
+        RxUtils.createObserable();
+
     }
 }
